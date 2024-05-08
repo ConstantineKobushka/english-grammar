7dbdef4b507f539f5543854fb80215f54ec32f84
Constantine Kobushka <kobushkaconstantine@gmail.com>
2024-05-08 19:47:28 +0300

    add html, css and js

--- js/main.js
+++ js/main.js
@@ -46,3 +46,58 @@ lessonNavigationLists.forEach((lessonNavigationList) =>
     curentblock.classList.remove('hidden');
   })
 );
+
+//! Puzzle
+
+const slider = document.querySelector('.slider');
+const sliderItems = Array.from(slider.children);
+const btnNex = document.querySelector('.btn-next');
+const btnAgain = document.querySelector('.btn-again');
+
+function init() {
+  sliderItems.forEach(function (slide, index) {
+    // Скрываем кнопку 'почати знову'
+    btnAgain.classList.add('hidden');
+    // Удаляем класс hidden c первого єлемента
+    sliderItems[0].classList.remove('hidden');
+    // Добавляем кнопку btnNext
+    btnNex.classList.remove('hidden');
+    // Скрываем все слайды кроме первого
+    if (index !== 0) slide.classList.add('hidden');
+    // Добавляем атрибут data-index со значением index
+    slide.dataset.index = index;
+    // Добавляем атрибут data-active для первого / активного слайда
+    sliderItems[0].setAttribute('data-active', '');
+  });
+}
+
+init();
+
+let nextSlideIndex;
+
+function showNextSlide() {
+  // Скрываем текущий слайд
+  const currentSlide = slider.querySelector('[data-active]');
+  const currentSlideIndex = +currentSlide.dataset.index;
+  currentSlide.classList.add('hidden');
+  currentSlide.removeAttribute('data-active');
+  // let nextSlideIndex;
+  nextSlideIndex = currentSlideIndex + 1;
+  const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
+  nextSlide.classList.remove('hidden');
+  nextSlide.setAttribute('data-active', '');
+}
+
+btnNex.addEventListener('click', function () {
+  if (nextSlideIndex === sliderItems.length - 2) {
+    showNextSlide();
+    btnNex.classList.add('hidden');
+    btnAgain.classList.remove('hidden');
+    return;
+  }
+  showNextSlide();
+});
+
+btnAgain.addEventListener('click', function () {
+  init();
+});
